b8086905faeb8fed87b9864618a508335f7b8b35
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-05-12 17:00:49 -0500

    [add] .gitnore and [update] package.json/server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-server.js
+var express = require('express'),
+	server = express(),
+	io = require('socket.io')(server);
+
+server.get('/', function (req, res) {
+	res.send('Hey! This is Express with Socket.io :)')
+})
+
+server.listen(3000, function () {
+	console.log('Server running --> http://localhost:3000')
+})
