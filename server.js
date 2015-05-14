2caf4bebd78c04314ee380600f53247eacd49930
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-05-14 09:56:07 -0500

    [add] views, swig and [update] server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var server = require('express')(),
 	app = require('http').Server(server),
-	io = require('socket.io')(app);
+	io = require('socket.io')(app),
+	swig = require('swig');
+
+server.engine('html', swig.renderFile);
+server.set('view engine', 'html');
+server.set('views', './views');
 
 server.get('/', function (req, res) {
-	res.send('Hey! This is Express with Socket.io :)')
+	res.render('index')
 })
 
 server.listen(28386, function () {
